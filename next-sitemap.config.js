c1c65790b72c0f36bfae4e514db334a93d918ca2
Aditya Jain <adityajain907935@gmail.com>
2026-04-09 19:48:22 +0530

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://aysolarenergy.in/', // ⚠️ apna real domain daal
+  generateRobotsTxt: true,
+};
+
+export default config;
